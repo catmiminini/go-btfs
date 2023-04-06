aac5a856a81d32f398827ded47a2e882605550f2
shawn <shawn.huang@tron.network>
2023-04-06 21:12:59 +0800

    fix: increase the inbound conns of resource manager

--- core/node/libp2p/rcmgr_defaults.go
+++ core/node/libp2p/rcmgr_defaults.go
@@ -63,14 +63,14 @@ func createDefaultLimitConfig(cfg config.SwarmConfig) (rcmgr.LimitConfig, error)
 
 			// By default, we just limit connections on the inbound side.
 			Conns:         bigEnough,
-			ConnsInbound:  rcmgr.DefaultLimits.SystemBaseLimit.ConnsInbound, // same as libp2p default,
+			ConnsInbound:  2 << 10,
 			ConnsOutbound: bigEnough,
 
 			// We limit streams since they not only take up memory and CPU.
 			// The Memory limit protects us on the memory side,
 			// but a StreamsInbound limit helps protect against unbound CPU consumption from stream processing.
 			Streams:         bigEnough,
-			StreamsInbound:  rcmgr.DefaultLimits.SystemBaseLimit.StreamsInbound,
+			StreamsInbound:  (2 << 10) * 16,
 			StreamsOutbound: bigEnough,
 		},
 		// Most limits don't see an increase because they're already infinite/bigEnough or at their max value.
